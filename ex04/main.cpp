e33fcb97250751101ea7b2f54f031e2f2a5a612d
Guillaume GILBERT <ggilbert@e2r11p9.clusters.42paris.fr>
2022-02-17 13:28:59 +0100

    ex04 ok

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,18 +1,100 @@
 #include <iostream>
+#include <fstream>
 
-int main( int ac, char **av )
+bool arg_check(int ac, char **av)
 {
     if (ac == 4)
     {
-        std::string     s1 = av[2];
-        std::string     s2 = av[3];
+        std::string file = av[1];
+        std::string s1 = av[2];
+        std::string s2 = av[3];
+        if (file == "" || s1 == "" || s2 == "" ||
+            s1.compare(s2) == 0)
+            return (false);
+        return (true);
+    }
+    return (false);
+}
 
-        std::cout << s1 <<  "  " << s1.length() << std::endl << s2 << "  " << s2.length() << std::endl;
+std::string file_name_suffixe(std::string file)
+{
+    std::string new_name = file;
+
+    new_name += ".replace";
+    std::cout << new_name << std::endl;
+    return (new_name);
+}
+
+void my_find_replace(std::string *origin, std::string to_find, std:: string to_replace)
+{
+    size_t      pos;
+
+    pos = origin->find(to_find);
+    while (pos != origin->npos)
+    {
+        origin->erase(pos, to_find.length());
+        origin->insert(pos, to_replace);
+        pos = origin->find(to_find, (pos + to_replace.length()));
+    }
+}
+
+bool    replacer(std::string file, std::string s1, std::string s2)
+{
+    std::ifstream       ifs(file.c_str());
+    std::ofstream       ofs;
+    std::string         replace_file = file_name_suffixe(file);
+    std::string         str;
+
+    if (ifs.fail())
+        return (false);
+    ofs.open(replace_file.c_str(), std::ofstream::trunc);
+    if (ofs.fail())
+        return (false);
+    while (!ifs.eof())
+    {
+        if (ifs.fail())
+            return (false);
+        getline(ifs, str);
+        my_find_replace(&str, s1, s2);
+        ofs << str;
+        if(!ifs.eof())
+            ofs << std::endl;
+    }
+    ifs.close();
+    ofs.close();
+    return (true);
+}
+
+bool    fileexist(char *file)
+{
+    std::ifstream   ifs(file);
+
+    if (!ifs.is_open())
+        return (false);
+    ifs.close();
+    return (true);
+}
+
+int main( int ac, char **av )
+{
+    if (!arg_check(ac, av))
+    {
+        std::cout << "Error bad arguments" << std::endl;
         return (0);
     }
-    std::cout << "Bad number of arguments" << std::endl;
+    if (!fileexist(av[1]))
+    {
+        std::cout << "File error : can't open [" << av[1] <<  "]" << std::endl;
+        return (0);
+    }
+    std::string     file = av[1];
+    std::string     s1 = av[2];
+    std::string     s2 = av[3];
+
+    if (!replacer(file, s1, s2))
+        std::cout << "Error while reading file" << std::endl;
     return (0);
 }
 
-// use string.insert() and string.erase()
-// string.find()
+// use string.insert(position, string) and string.erase(start, end)
+// string.find() and string.compare()
---
+++ ex04/test.sh
@@ -0,0 +1,35 @@
+NAME="replacer"
+FILES_FOLDER="./files/"
+SUFFIXE=".replace"
+
+echo "Agruments errors\n"
+./${NAME}
+./${NAME} roger "" rabbit
+./${NAME} roger "poeut" ""
+./${NAME} roger rabbit rabbit
+
+echo "\n\nFile errors\n"
+./${NAME} roger lapin rabbit
+chmod 000 "${FILES_FOLDER}no_perm.txt"
+./${NAME} "${FILES_FOLDER}no_perm.txt" "pouet" "lol"
+chmod 644 "${FILES_FOLDER}no_perm.txt"
+
+echo "\n\nWorking\n"
+./${NAME} "${FILES_FOLDER}lol.txt" "ce" "ca"
+diff "${FILES_FOLDER}lol.txt" "${FILES_FOLDER}lol.txt${SUFFIXE}"
+
+echo "\n"
+./${NAME} "${FILES_FOLDER}au_cabaret_vert.txt" "Arthur Rimbaud" "Roger Rabbit"
+diff "${FILES_FOLDER}au_cabaret_vert.txt" "${FILES_FOLDER}au_cabaret_vert.txt${SUFFIXE}"
+
+echo "\n"
+./${NAME} "${FILES_FOLDER}lorem512_char_line.txt" "Lorem" "Ce texte n'a aucun sens"
+diff "${FILES_FOLDER}lorem512_char_line.txt" "${FILES_FOLDER}lorem512_char_line.txt${SUFFIXE}" 
+
+echo "\n"
+./${NAME} "${FILES_FOLDER}out.txt" "etlo" "hahahahaha"
+diff "${FILES_FOLDER}out.txt" "${FILES_FOLDER}out.txt${SUFFIXE}"
+
+echo "\n"
+./${NAME} "${FILES_FOLDER}la.txt" "la" "sol"
+#diff "${FILES_FOLDER}la.txt" "${FILES_FOLDER}la.txt${SUFFIXE}" 
